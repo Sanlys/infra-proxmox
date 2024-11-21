f6fbb1253a349c122fb8a6099178fd116466ecef
Sander Lysakermoen <slysakermoen@gmail.com>
2024-11-21 10:26:28 +0100

    .

--- main.tf
+++ main.tf
@@ -6,8 +6,7 @@ terraform {
       version = "3.0.1-rc4"
     }
     talos = {
-      source  = "siderolabs/talos"
-      version = "0.7.0-alpha.0"
+      source = "siderolabs/talos"
     }
   }
 }
@@ -125,6 +124,12 @@ resource "proxmox_vm_qemu" "node" {
 
 locals {
   ips = [for vm in proxmox_vm_qemu.node : vm.ssh_host]
+  controlplaneips = [
+    for vm in proxmox_vm_qemu.node :
+    vm.ssh_host
+    if can(regex("(?i)control-plane", vm.tags))
+  ]
+  workerips = tolist(setsubtract(local.ips, local.controlplaneips))
 }
 
 output "ips" {
@@ -132,6 +137,94 @@ output "ips" {
   description = "All node IPs"
 }
 
-locals {
+output "controlplaneips" {
+  value       = local.controlplaneips
+  description = "Control plane IPs"
+}
 
+output "workerips" {
+  value       = local.workerips
+  description = "Worker IPs"
+}
+
+/* */
+
+resource "talos_machine_secrets" "all_nodes" {}
+
+data "talos_machine_configuration" "controlplane" {
+  cluster_name     = "infra"
+  machine_type     = "controlplane"
+  cluster_endpoint = "https://${element(local.controlplaneips, 0)}:6443"
+  machine_secrets  = talos_machine_secrets.all_nodes.machine_secrets
+  config_patches = [
+    yamlencode([
+      {
+        op   = "add"
+        path = "/machine/install"
+        value = {
+          image = "factory.talos.dev/installer/ce4c980550dd2ab1b17bbf2b08801c7eb59418eafe8f279833297925d67c7515:v1.8.3"
+        }
+      },
+      {
+        op   = "add"
+        path = "/machine/services/-"
+        value = {
+          name    = "qemu-guest-agent"
+          command = "/usr/bin/qemu-ga"
+          args    = ["--method=virtio-serial", "--path=/dev/virtio-ports/org.qemu.guest_agent.0"]
+          enabled = true
+        }
+      }
+    ])
+  ]
+}
+
+data "talos_client_configuration" "controlplane" {
+  cluster_name         = "infra"
+  client_configuration = talos_machine_secrets.all_nodes.client_configuration
+  nodes                = local.controlplaneips
+}
+
+resource "talos_machine_configuration_apply" "controlplane" {
+  count                       = length(local.controlplaneips)
+  client_configuration        = data.talos_client_configuration.controlplane.client_configuration
+  machine_configuration_input = data.talos_machine_configuration.controlplane.machine_configuration
+  endpoint                    = element(local.controlplaneips, count.index)
+  node                        = element(local.controlplaneips, count.index)
+}
+
+data "talos_machine_configuration" "worker" {
+  cluster_name     = "infra"
+  machine_type     = "worker"
+  cluster_endpoint = "https://${element(local.controlplaneips, 0)}:6443"
+  machine_secrets  = talos_machine_secrets.all_nodes.machine_secrets
+}
+
+data "talos_client_configuration" "worker" {
+  cluster_name         = "infra"
+  client_configuration = talos_machine_secrets.all_nodes.client_configuration
+  nodes                = local.controlplaneips
+}
+
+resource "talos_machine_configuration_apply" "worker" {
+  count                       = length(local.workerips)
+  client_configuration        = data.talos_client_configuration.worker.client_configuration
+  machine_configuration_input = data.talos_machine_configuration.worker.machine_configuration
+  endpoint                    = element(local.workerips, count.index)
+  node                        = element(local.workerips, count.index)
+}
+
+resource "talos_machine_bootstrap" "controlplane" {
+  depends_on = [talos_machine_configuration_apply.controlplane]
+
+  count                = length(local.controlplaneips)
+  client_configuration = data.talos_client_configuration.controlplane.client_configuration
+  endpoint             = element(local.controlplaneips, count.index)
+  node                 = element(local.controlplaneips, count.index)
+}
+
+resource "talos_cluster_kubeconfig" "this" {
+  depends_on           = [talos_machine_bootstrap.controlplane]
+  client_configuration = data.talos_client_configuration.controlplane.client_configuration
+  node                 = local.controlplaneips[0]
 }
